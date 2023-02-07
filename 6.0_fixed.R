5965ad80b7197dd72e70b7c2dfb865d02ab83003
elhumble <emily.lhumble@gmail.com>
2023-02-07 11:23:08 +0000

    polarisation

--- 6.0_fixed.R
+++ 6.0_fixed.R
@@ -2,60 +2,41 @@ library(data.table)
 library(dplyr)
 library(ggplot2)
 library(wesanderson)
-source("scripts/theme_emily.R")
+library(tidyr)
+library(purrr)
+source("theme_emily.R")
 
-# Script to count fixed putative deleterious alleles per pop
+# Script to calculate the proportion of homozygous derived genotypes due to alleles fixed in each population
 
-#scp ehumble@eddie.ecdf.ed.ac.uk:/exports/cmvm/eddie/eb/groups/ogden_grp/emily/SHO_reseq_2022/data/out/6_load/DS_NS/snpeff/ORYX_geno_DS_NS_biallelic_chr_mdepth_miss_maf_polarised_annotated_filtered_missense_snps.frq.strat data/out/6_load/DS_NS/snpeff/
-#scp ehumble@eddie.ecdf.ed.ac.uk:/exports/cmvm/eddie/eb/groups/ogden_grp/emily/SHO_reseq_2022/data/out/6_load/DS_NS/snpeff/ORYX_geno_DS_NS_biallelic_chr_mdepth_miss_maf_polarised_annotated_filtered_lof_snps.frq.strat data/out/6_load/DS_NS/snpeff/
+# scp ehumble@eddie.ecdf.ed.ac.uk:/exports/cmvm/eddie/eb/groups/ogden_grp/emily/SHO_reseq_2022/data/out/6_load/DS_NS/snpeff/freqs/*afreq data/out/6_load/DS_NS/snpeff/freqs/
+
+#~~ Read in files
   
-col_palette <- c(wes_palette("Cavalcanti1")[2],
-                   wes_palette("Cavalcanti1")[3],
-                   wes_palette("Cavalcanti1")[1],
-                   wes_palette("GrandBudapest1")[4])
+data_path <- "data/out/6_load/DS_NS/snpeff/freqs/"
+files <- dir(data_path, pattern = "*\\.afreq")
 
-missense_frq <- fread("data/out/6_load/DS_NS/snpeff/ORYX_geno_DS_NS_biallelic_chr_mdepth_miss_maf_polarised_annotated_filtered_missense_snps.frq.strat")
+# Visualise freqs for each locus across populations
 
-hist(missense_frq$MAF)
+freqs <- tibble(filename = files) %>%
+  mutate(file_contents = map(filename,
+                             ~ fread(file.path(data_path, .)))) %>%
+  unnest(cols = c(file_contents)) %>%
+  mutate(filename = gsub("ORYX_geno_DS_NS_biallelic_chr_mdepth_miss_maf_polarised_annotated_filtered_", 
+                         "", filename)) %>%
+  separate(filename, c("snp_class", "Origin"), sep = "_snps_") %>%
+  mutate(Origin = gsub(".afreq", "", Origin))
 
-missense_frq %>%
-  group_by(CLST) %>%
-  filter(MAF >0.5) %>%
-  summarise(sum = n())
+#~~ Alt freq distribution
 
-missense_frq %>%
-  filter(MAF != 0) %>%
-  group_by(CLST) %>%
-  summarise(mean = mean(MAF))
+ggplot(freqs, aes(ALT_FREQS)) +
+  geom_histogram(bins = 15) + 
+  facet_wrap(Origin~snp_class, scales = "free") +
+  theme_emily()
 
-ggplot(filter(missense_frq, MAF != 0), aes(MAF, fill = CLST)) +
-  geom_histogram(bins = 15) + facet_wrap(~CLST) +
-  theme_emily() + scale_fill_manual(values =  col_palette) +
-  ggtitle("Missense")
-
-lof_frq <- fread("data/out/6_load/DS_NS/snpeff/ORYX_geno_DS_NS_biallelic_chr_mdepth_miss_maf_polarised_annotated_filtered_lof_snps.frq.strat")
-
-hist(lof_frq$MAF)
-
-lof_frq %>%
-  group_by(CLST) %>%
-  filter(MAF == 1) %>%
-  summarise(sum = n())
-
-lof_frq %>%
-  filter(MAF != 0) %>%
-  group_by(CLST) %>%
-  summarise(mean = mean(MAF))
-
-ggplot(filter(lof_frq, MAF != 0), aes(MAF, fill = CLST)) +
-  geom_histogram(bins = 15) + facet_wrap(~CLST) +
-  theme_emily() + scale_fill_manual(values =  col_palette) +
-  ggtitle("LoF")
-
-# join freq dfs
-
-freqs <- rbind(missense_frq, lof_frq)
-
+ggplot(filter(freqs, ALT_FREQS > 0), aes(ALT_FREQS)) +
+  geom_histogram(bins = 15) + 
+  facet_wrap(Origin~snp_class, scales = "free") +
+  theme_emily()
 
 #~~ Read in genotype data for SNP classes
 
@@ -73,35 +54,39 @@ meta <- fread("data/meta/SHO_WGS_IDs_metadata_clean.csv") %>%
 data_path <- "data/out/6_load/DS_NS/snpeff/"
 files <- dir(data_path, pattern = "*\\.traw")
 
-# Number of 0,1,2 genotypes per individual across SNP classes
+# Number of 0,1,2 genotypes per individual for lof and missense
 
-df <- tibble(filename = files[c(2:3)]) %>%
+df <- tibble(filename = files[c(3:4)]) %>%
   mutate(file_contents = map(filename,
                              ~ fread(file.path(data_path, .)))) %>%
   unnest(cols = c(file_contents)) %>%
   select(-c(CHR, `(C)M`, POS, COUNTED, ALT)) %>%
   pivot_longer(cols = -c(filename, SNP)) %>%
-  mutate(name = gsub("_.+", "", name)) %>%
+  mutate(name = gsub("0_", "", name)) %>%
   left_join(meta, by = c("name" = "Sample_ID")) %>%
   filter(name != "MSH682" & name != "MSH238" & name != "MSH250") # related and admixed
 
+# Missense
+
 EAD_A_missense <- filter(df, Origin == "EAD A") %>%
-  filter(value == 2) %>%
-  filter(filename == "ORYX_geno_DS_NS_biallelic_chr_mdepth_miss_maf_polarised_annotated_filtered_missense_snps.traw") %>%
-  left_join(filter(missense_frq, CLST=="EAD"), by = "SNP") %>%
-  mutate(fixed = case_when(MAF == 1 ~ "Fixed",
+  filter(value == 0) %>%
+  filter(filename == 
+           "ORYX_geno_DS_NS_biallelic_chr_mdepth_miss_maf_polarised_annotated_filtered_missense_snps.traw") %>%
+  left_join(filter(freqs, Origin == "EAD_A" & snp_class == "missense"), by = c("SNP" = "ID")) %>%
+  mutate(fixed = case_when(ALT_FREQS == 1 ~ "Fixed",
                            TRUE ~ "Variable"))
 
 EAD_A_missense %>%
   group_by(fixed) %>%
   summarise(n = n()) %>%
-  mutate(Freq = n/sum(n))
+  mutate(Freq = n/sum(n)*100)
 
 EAD_B_missense <- filter(df, Origin == "EAD B") %>%
-  filter(value == 2) %>%
-  filter(filename == "ORYX_geno_DS_NS_biallelic_chr_mdepth_miss_maf_polarised_annotated_filtered_missense_snps.traw") %>%
-  left_join(filter(freqs, CLST=="Hilwa"), by = "SNP") %>%
-  mutate(fixed = case_when(MAF == 1 ~ "Fixed",
+  filter(value == 0) %>%
+  filter(filename == 
+           "ORYX_geno_DS_NS_biallelic_chr_mdepth_miss_maf_polarised_annotated_filtered_missense_snps.traw") %>%
+  left_join(filter(freqs, Origin == "EAD_B" & snp_class == "missense"), by = c("SNP" = "ID")) %>%
+  mutate(fixed = case_when(ALT_FREQS == 1 ~ "Fixed",
                            TRUE ~ "Varible"))
 
 EAD_B_missense %>% 
@@ -111,10 +96,10 @@ EAD_B_missense %>%
 
 
 USA_missense <- filter(df, Origin == "USA") %>%
-  filter(value == 2) %>%
+  filter(value == 0) %>%
   filter(filename == "ORYX_geno_DS_NS_biallelic_chr_mdepth_miss_maf_polarised_annotated_filtered_missense_snps.traw") %>%
-  left_join(filter(freqs, CLST=="USA"), by = "SNP") %>%
-  mutate(fixed = case_when(MAF == 1 ~ "Fixed",
+  left_join(filter(freqs, Origin == "USA" & snp_class == "missense"), by = c("SNP" = "ID")) %>%
+  mutate(fixed = case_when(ALT_FREQS == 1 ~ "Fixed",
                            TRUE ~ "Varible"))
 
 USA_missense %>%
@@ -123,10 +108,11 @@ USA_missense %>%
   mutate(Freq = n/sum(n)*100)
 
 EEP_missense <- filter(df, Origin == "EEP") %>%
-  filter(value == 2) %>%
-  filter(filename == "ORYX_geno_DS_NS_biallelic_chr_mdepth_miss_maf_polarised_annotated_filtered_missense_snps.traw") %>%
-  left_join(filter(freqs, CLST=="EEP"), by = "SNP") %>%
-  mutate(fixed = case_when(MAF == 1 ~ "Fixed",
+  filter(value == 0) %>%
+  filter(filename == 
+           "ORYX_geno_DS_NS_biallelic_chr_mdepth_miss_maf_polarised_annotated_filtered_missense_snps.traw") %>%
+  left_join(filter(freqs, Origin == "EEP" & snp_class == "missense"), by = c("SNP" = "ID")) %>%
+  mutate(fixed = case_when(ALT_FREQS == 1 ~ "Fixed",
                            TRUE ~ "Varible"))
 
 EEP_missense %>%
@@ -136,35 +122,11 @@ EEP_missense %>%
 
 # LoF
 
-EEP_lof<- filter(df, Origin == "EEP") %>%
-  filter(value == 2) %>%
-  filter(filename == "ORYX_geno_DS_NS_biallelic_chr_mdepth_miss_maf_polarised_annotated_filtered_lof_snps.traw") %>%
-  left_join(filter(freqs, CLST=="EEP"), by = "SNP") %>%
-  mutate(fixed = case_when(MAF == 1 ~ "Fixed",
-                           TRUE ~ "Varible"))
-
-EEP_lof %>%
-  group_by(fixed) %>%
-  summarise(n = n()) %>%
-  mutate(Freq = n/sum(n)*100)
-
-USA_lof<- filter(df, Origin == "USA") %>%
-  filter(value == 2) %>%
-  filter(filename == "ORYX_geno_DS_NS_biallelic_chr_mdepth_miss_maf_polarised_annotated_filtered_lof_snps.traw") %>%
-  left_join(filter(freqs, CLST=="USA"), by = "SNP") %>%
-  mutate(fixed = case_when(MAF == 1 ~ "Fixed",
-                           TRUE ~ "Varible"))
-
-USA_lof %>%
-  group_by(fixed) %>%
-  summarise(n = n()) %>%
-  mutate(Freq = n/sum(n)*100)
-
 EAD_A_lof<- filter(df, Origin == "EAD A") %>%
-  filter(value == 2) %>%
+  filter(value == 0) %>%
   filter(filename == "ORYX_geno_DS_NS_biallelic_chr_mdepth_miss_maf_polarised_annotated_filtered_lof_snps.traw") %>%
-  left_join(filter(freqs, CLST=="EAD"), by = "SNP") %>%
-  mutate(fixed = case_when(MAF == 1 ~ "Fixed",
+  left_join(filter(freqs, Origin == "EAD_A" & snp_class == "lof"), by = c("SNP" = "ID")) %>%
+  mutate(fixed = case_when(ALT_FREQS == 1 ~ "Fixed",
                            TRUE ~ "Varible"))
 
 EAD_A_lof %>%
@@ -173,13 +135,41 @@ EAD_A_lof %>%
   mutate(Freq = n/sum(n)*100)
 
 EAD_B_lof<- filter(df, Origin == "EAD B") %>%
-  filter(value == 2) %>%
+  filter(value == 0) %>%
   filter(filename == "ORYX_geno_DS_NS_biallelic_chr_mdepth_miss_maf_polarised_annotated_filtered_lof_snps.traw") %>%
-  left_join(filter(freqs, CLST=="Hilwa"), by = "SNP") %>%
-  mutate(fixed = case_when(MAF == 1 ~ "Fixed",
+  left_join(filter(freqs, Origin == "EAD_B" & snp_class == "lof"), by = c("SNP" = "ID")) %>%
+  mutate(fixed = case_when(ALT_FREQS == 1 ~ "Fixed",
                            TRUE ~ "Varible"))
 
 EAD_B_lof %>%
   group_by(fixed) %>%
   summarise(n = n()) %>%
   mutate(Freq = n/sum(n)*100)
+
+
+USA_lof<- filter(df, Origin == "USA") %>%
+  filter(value == 0) %>%
+  filter(filename == "ORYX_geno_DS_NS_biallelic_chr_mdepth_miss_maf_polarised_annotated_filtered_lof_snps.traw") %>%
+  left_join(filter(freqs, Origin == "USA" & snp_class == "lof"), by = c("SNP" = "ID")) %>%
+  mutate(fixed = case_when(ALT_FREQS == 1 ~ "Fixed",
+                           TRUE ~ "Varible"))
+
+USA_lof %>%
+  group_by(fixed) %>%
+  summarise(n = n()) %>%
+  mutate(Freq = n/sum(n)*100)
+
+
+EEP_lof <- filter(df, Origin == "EEP") %>%
+  filter(value == 0) %>%
+  filter(filename == 
+           "ORYX_geno_DS_NS_biallelic_chr_mdepth_miss_maf_polarised_annotated_filtered_lof_snps.traw") %>%
+  left_join(filter(freqs, Origin == "EEP" & snp_class == "lof"), by = c("SNP" = "ID")) %>%
+  mutate(fixed = case_when(ALT_FREQS == 1 ~ "Fixed",
+                           TRUE ~ "Varible"))
+
+EEP_lof %>%
+  group_by(fixed) %>%
+  summarise(n = n()) %>%
+  mutate(Freq = n/sum(n)*100)
+
